14e0934190802ede1927a23cbe5d657ba57730ea
Carlos Hernandez <chernandez@teknei.com>
2024-09-12 22:41:30 -0600

    Add gitignore

---
+++ Prueba/MainView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  Prueba
+//
+//  Created by Administrador on 12/09/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
