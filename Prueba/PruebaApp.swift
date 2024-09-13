269075fc2571830a097363abfcd7a3cce45f580e
Carlos Hernandez <chernandez@teknei.com>
2024-09-12 22:30:30 -0600

    Initial Commit

---
+++ Prueba/PruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaApp.swift
+//  Prueba
+//
+//  Created by Administrador on 12/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
